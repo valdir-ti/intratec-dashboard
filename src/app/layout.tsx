72c36bd885c91edece95c52bc5a329a71f124ff9
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-07 19:12:19 -0300

    feat: add toaster props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ToastContainer 
+          autoClose={1500}
+          closeOnClick
+          closeButton
+        />
+        {children}
+      </body>
     </html>
   )
 }
