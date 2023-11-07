7a413f57b4e5f25242db153cc88432cdda707469
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-07 17:13:09 -0300

    chore: config colors and add libs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+     colors: {
+        accent: "#3AAF9F",
+        accentDark: "#297C71"
+     }
     },
   },
   plugins: [],
